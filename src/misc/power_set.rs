29870b7b072615304e013a0ded080538ce2bbda8
JJ Style <style.jj@pm.me>
2021-04-01 18:55:55 +0100

    take reference and copy values

--- src/misc/power_set.rs
+++ src/misc/power_set.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-pub fn power_set<T: Copy + Clone>(set: Vec<T>) -> Result<Vec<Vec<T>>, Box<dyn Error>> {
+pub fn power_set<T: Copy>(set: &Vec<T>) -> Result<Vec<Vec<T>>, Box<dyn Error>> {
     let set_length : u32  = set.len() as u32;
     let mut subsets = Vec::with_capacity(i32::pow(2, set_length) as usize);
     for i in 0..i32::pow(2,set_length) {
@@ -13,7 +13,7 @@ pub fn power_set<T: Copy + Clone>(set: Vec<T>) -> Result<Vec<Vec<T>>, Box<dyn Er
         for (idx,bit) in mask.chars().enumerate() {
             match bit {
                 '0' => (),
-                '1' => subset.push(set[idx].clone()),
+                '1' => subset.push(set[idx]),
                 e => return Err(Box::from(format!("expected binary value 1 or 0, got {:?}", e)))
             }
         }
@@ -29,7 +29,7 @@ mod tests {
     #[test]
     fn test_power_set() {
         let source = vec![1,2,3];
-        let source_ps = power_set(source).unwrap();
+        let source_ps = power_set(&source).unwrap();
         assert_eq!(source_ps.len(), 8);
         assert!(source_ps.contains(&vec![]));
         assert!(source_ps.contains(&vec![1]));
