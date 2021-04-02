507790fbbc4c7b32688abc01294516e35a5f1fba
JJ Style <style.jj@pm.me>
2021-04-02 13:24:23 +0100

    comments

--- src/misc/power_set.rs
+++ src/misc/power_set.rs
@@ -3,18 +3,22 @@ use std::error::Error;
 pub fn power_set<T: Copy>(set: &Vec<T>) -> Result<Vec<Vec<T>>, Box<dyn Error>> {
     let set_length : u32  = set.len() as u32;
     let mut subsets = Vec::with_capacity(i32::pow(2, set_length) as usize);
+
     for i in 0..i32::pow(2,set_length) {
         let mut subset: Vec<T> = Vec::new();
+
+        // create binary mask, pad with 0s as no dynamic string formatting
         let mut mask = format!("{:b}", i);
         while mask.len() != set.len() {
             mask.insert_str(0, "0");
         }
 
+        // 1 = in the subset, 0 = not in the subset
         for (idx,bit) in mask.chars().enumerate() {
             match bit {
                 '0' => (),
                 '1' => subset.push(set[idx]),
-                e => return Err(Box::from(format!("expected binary value 1 or 0, got {:?}", e)))
+                e => return Err(format!("expected binary value 1 or 0, got {:?}", e).into())
             }
         }
         subsets.push(subset);
