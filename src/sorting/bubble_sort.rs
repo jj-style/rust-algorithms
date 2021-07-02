2dbf181887311176741af066f89d23f60134ed22
JJ Style <style.jj@pm.me>
2021-07-02 23:17:35 +0100

    bubble sort take array and return vec

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -1,8 +1,8 @@
-pub fn bubble_sort<T>(vec: &Vec<T>) -> Vec<T>
+pub fn bubble_sort<T>(arr: &[T]) -> Vec<T>
 where
     T: Ord + Clone,
 {
-    let mut copy = vec.to_vec();
+    let mut copy = arr.to_vec();
     for i in 0..copy.len() {
         for j in 0..copy.len() - i - 1 {
             if copy[j] > copy[j + 1] {
@@ -19,23 +19,23 @@ mod tests {
 
     #[test]
     fn test_bubble_sort() {
-        let vec = vec![7, 1, 9, 4, 2, 3, 2, 6];
-        let sorted = bubble_sort(&vec);
+        let arr = [7, 1, 9, 4, 2, 3, 2, 6];
+        let sorted = bubble_sort(&arr);
         println!("sorted is {:?}", sorted);
         assert_eq!(sorted, vec![1, 2, 2, 3, 4, 6, 7, 9]);
     }
 
     #[test]
     fn test_bubble_sort_descending() {
-        let vec = vec![7, 6, 5, 4, 3, 2, 1];
-        let sorted = bubble_sort(&vec);
+        let arr = [7, 6, 5, 4, 3, 2, 1];
+        let sorted = bubble_sort(&arr);
         assert_eq!(sorted, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 
     #[test]
     fn test_bubble_sort_ascending() {
-        let vec = vec![1, 2, 3, 4, 5, 6, 7];
-        let sorted = bubble_sort(&vec);
+        let arr = [1, 2, 3, 4, 5, 6, 7];
+        let sorted = bubble_sort(&arr);
         assert_eq!(sorted, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 }
