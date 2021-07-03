dc9669fd22322d41ffbb26ecf2e9bfecac69e3de
JJ Style <style.jj@pm.me>
2021-07-03 17:28:19 +0100

    make quicksort pure

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use algorithms::sorting::quicksort;
 use algorithms::tree::BTNode;
 
 fn main() {
-    let unsorted = vec![5,8,1,3,2,8];
+    let unsorted = [5, 8, 1, 3, 2, 8];
     println!("Before: {:?}", unsorted);
-    println!("After : {:?}", quicksort(unsorted));
+    println!("After : {:?}", quicksort(&unsorted));
 
     let mut root = BTNode::new(5);
     root.insert_node(10);
--- src/sorting/quicksort.rs
+++ src/sorting/quicksort.rs
@@ -1,18 +1,19 @@
 use std::cmp::Ordering::{Equal, Greater, Less};
 
-pub fn quicksort<T>(arr: Vec<T>) -> Vec<T>
+fn _quicksort<T>(arr: Vec<T>) -> Vec<T>
 where
-    T: Ord + std::fmt::Debug + Clone,
+    T: Ord + Clone,
 {
     if arr.len() <= 1 {
         return arr;
     }
 
     let pivot = arr[0].clone();
+    let max_len = arr.len();
 
-    let mut less: Vec<T> = vec![];
-    let mut greater: Vec<T> = vec![];
-    let mut equal: Vec<T> = vec![];
+    let mut less: Vec<T> = Vec::new();
+    let mut greater: Vec<T> = Vec::new();
+    let mut equal: Vec<T> = Vec::new();
 
     for item in arr.into_iter() {
         match item.cmp(&pivot) {
@@ -22,11 +23,21 @@ where
         }
     }
 
-    let mut less_s = quicksort(less);
-    let mut greater_s = quicksort(greater);
-    less_s.append(&mut equal);
-    less_s.append(&mut greater_s);
-    less_s
+    let mut sorted = Vec::with_capacity(max_len);
+    let mut less_sorted = _quicksort(less);
+    let mut greater_sorted = _quicksort(greater);
+
+    sorted.append(&mut less_sorted);
+    sorted.append(&mut equal);
+    sorted.append(&mut greater_sorted);
+    sorted
+}
+
+pub fn quicksort<T>(arr: &[T]) -> Vec<T>
+where
+    T: Ord + Clone,
+{
+    _quicksort(arr.to_vec())
 }
 
 #[cfg(test)]
@@ -36,21 +47,21 @@ mod tests {
     #[test]
     fn test_quicksort() {
         let arr = [7, 1, 9, 4, 2, 3, 2, 6];
-        let sorted_arr = quicksort(arr.to_vec());
-        assert_eq!(sorted_arr, [1, 2, 2, 3, 4, 6, 7, 9]);
+        let sorted_arr = quicksort(&arr);
+        assert_eq!(sorted_arr, vec![1, 2, 2, 3, 4, 6, 7, 9]);
     }
 
     #[test]
     fn test_quicksort_descending() {
         let arr = [7, 6, 5, 4, 3, 2, 1];
-        let sorted_arr = quicksort(Vec::from(arr));
-        assert_eq!(sorted_arr, [1, 2, 3, 4, 5, 6, 7]);
+        let sorted_arr = quicksort(&arr);
+        assert_eq!(sorted_arr, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 
     #[test]
     fn test_quicksort_ascending() {
         let arr = [1, 2, 3, 4, 5, 6, 7];
-        let sorted_arr = quicksort(Vec::from(arr));
-        assert_eq!(sorted_arr, [1, 2, 3, 4, 5, 6, 7]);
+        let sorted_arr = quicksort(&arr);
+        assert_eq!(sorted_arr, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 }
